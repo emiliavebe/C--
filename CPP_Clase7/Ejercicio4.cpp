aa034d994bbbeff4f4e8f30865bfbb43e639f1f8
emiliavebe <emiliavebe@gmail.com>
2026-09-12 21:28:22 -0500

    Usar una variable adicional para intercambiar los 2 valores

--- CPP_Clase7/Ejercicio4.cpp
+++ CPP_Clase7/Ejercicio4.cpp
@@ -40,45 +40,52 @@ using namespace std;
 
 //PROTOTIPADO
 
-void LeerDatos(int num1, int num2);
+void LeerNumeros(int &num1, int &num2);
 void MostrarDatos(int num1, int num2);
-int HacerIntercambio(int x, int y);
+void Intercambiar(int &x, int &y);
 void MostrarIntercambio(int num1, int num2);
-
 
 
 int main(){
 
     int num1, num2;
 
-    LeerDatos(num1, num2);
+    LeerNumeros(num1, num2);
     MostrarDatos(num1, num2);
-    (num2 = num1 + num2, num1 = num2 - num1, num2 = num2 - num1) = HacerIntercambio(num1, num2);
+    Intercambiar(num1, num2);
     MostrarIntercambio(num1, num2);
 
 }
 
-void LeerDatos(int num1, int num2){
+void LeerNumeros(int &num1, int &num2){
 
-    cout << "Ingrese 2 numeros enteros: "<< endl;
-    cin >> num1 >> num2;
+    cout << "Ingrese número entero 1: "<< endl;
+    cin >> num1;
+    cout << "Ingrese número entero 2: "<< endl;
+    cin >> num2;
 }
 
 void MostrarDatos(int num1, int num2){
 
-    cout << "Numero 1: " << num1 << endl;
-    cout << "Numero 2: " << num2 << endl;
+    cout << "***Valores antes del intercambio***" << endl;
+    cout << endl;
+    cout << "Número 1: " << num1 << endl;
+    cout << "Número 2: " << num2 << endl;
 }
 
-int HacerIntercambio(int x, int y){
+void Intercambiar(int &x, int &y){
 
-    return y = x + y;
-           x = y - x;
-           y = y - x;
+    int r = x;
+    x = y;
+    y = r;
+
 }
 
 void MostrarIntercambio(int num1, int num2){
 
-    cout << "El valor del numero 1 es: " << num1 << endl;
-    cout << "El vaor del numero 2 es: " << num2;
+    cout << "***Valores despues del intercambio***" << endl;
+    cout << endl;
+    cout << "Número 1: " << num1 << endl;
+    cout << "Número 2: " << num2 << endl;
+   
 }
